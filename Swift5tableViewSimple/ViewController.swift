72deafc90a03feb164889c71e515db06fda05e7a
Hajime Nutahara <hajime.nutahara@gmail.com>
2020-07-14 12:00:04 +0900

    Initial Commit

---
+++ Swift5tableViewSimple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5tableViewSimple
+//
+//  Created by nhajime on 2020/07/14.
+//  Copyright © 2020 Hajime Nutahara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
